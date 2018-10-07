c3fd1b4e8469168548a520a3783acc9c549a045c
Ihor <vippwp7@gmail.com>
2018-10-07 23:49:03 +0300

    test

--- js/script.js
+++ js/script.js
@@ -26,6 +26,15 @@ $('.main-fifth__slider').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     infinite:true,
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            arrows:false,
+            appendDots:$('.main-fifth__dots'),
+          }
+        },
+      ],
 });
 
 $('.sixth-slider__nav').slick({
@@ -35,17 +44,60 @@ $('.sixth-slider__nav').slick({
   arrows:true,
   centerMode: true,
   vertical: true,
-  focusOnSelect: true,
+  speed: 250,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        dots: true,
+        infinite:false,
+        arrows:false,
+        vertical: false,
+        centerMode: false,
+        appendDots:$('.dots-mobile'),
+      }
+    },
+  ],
 });
 
+$('.main-ninth__slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true,
+  arrows:true,
+  infinite:true,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        arrows:false,
+        appendDots:$('.main-ninth__dots'),
+      }
+    },
+  ],
+});
+
+
+$('.sixth-slider__nav').on('afterChange', function(event, slick, currentSlide, nextSlide){
+  var currentSrc = $('.sixth-slider__nav .slick-current').attr('src');
+  $('.sixth-slider__current .active').removeClass('active').siblings('.sixth-slider__image').addClass('active').attr('src',currentSrc);
+});
 
 $(document).ready(function() {
   var currentSrc = $('.sixth-slider__nav .slick-current').attr('src');
-  $('.sixth-slider__active').attr('src',currentSrc);
+  $('.sixth-slider__image').attr('src',currentSrc);
 });
 
-$('.sixth-slider__nav button,.sixth-slider__nav img').click(function(){
-  var currentSrc = $('.sixth-slider__nav .slick-current').attr('src');
-  $('.sixth-slider__active').attr('src',currentSrc);
-  console.log(currentSrc);
+$('.header-content .dogs-btn__smpl').click(function(){
+  $('.header-modal , body , html').addClass('active');
+});
+
+$('.main-eighth__form .close , .form-thx q').click(function(){
+  $(this).parents('.header-modal').removeClass('active');
+  $('.form-thx, body , html').removeClass('active');
+});
+
+$('.hamburger').click(function(){
+  $(this).toggleClass('active');
+  $('.header-content__list').toggleClass('active');
 });
