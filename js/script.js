0149eb90d9d98843238518f0c477d8ec80b50b11
Ihor <vippwp7@gmail.com>
2018-10-08 21:51:08 +0300

    pre final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$(document).on("click",".header-content__item a , .main-frst__buy a , .header-content__img a ,.card-wrapper a" , function (event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+        top = $(id).offset().top + 10;
+        $('body,html').animate({scrollTop: top}, 500);
+});
 $('.switch').click(function(){
   $('.main-thrd__house').toggleClass('active');
   $('.hint-btn').removeClass('active');
@@ -7,6 +13,27 @@ $('.hint-btn').click(function(){
   $(this).toggleClass('active');
   $('.hint-btn').not(this).removeClass('active');
 });
+
+$(window).on("scroll", function(){
+  dogParEffect();
+});
+
+dogParEffect();
+
+function dogParEffect(){
+  var scrollHeight = $(this).scrollTop();
+  
+  var coeficientImg = 0.07;
+  var coeficientBg = 0.05;
+  
+  var imgTop = scrollHeight*coeficientImg*(1);
+  var resultBgTop = scrollHeight*coeficientBg*(-1);
+
+  if (scrollHeight < 600) {
+    $('.main-frst__house').css("top",resultBgTop);
+    $('.dog-house-img').css("margin-top",imgTop);
+  } 
+}
 
 $('.slider-container').slick({
     dots: true,
@@ -45,13 +72,12 @@ $('.sixth-slider__nav').slick({
   centerMode: true,
   vertical: true,
   speed: 250,
+  focusOnSelect: true,
   responsive: [
     {
       breakpoint: 960,
       settings: {
         dots: true,
-        infinite:false,
-        arrows:false,
         vertical: false,
         centerMode: false,
         appendDots:$('.dots-mobile'),
@@ -100,4 +126,16 @@ $('.main-eighth__form .close , .form-thx q').click(function(){
 $('.hamburger').click(function(){
   $(this).toggleClass('active');
   $('.header-content__list').toggleClass('active');
+});
+
+$(document).on("swipeleft",'.sixth-slider__image', function(event){
+  $('.sixth-slider__nav .slick-next').click();
+});
+$(document).on("swiperight",'.sixth-slider__image', function(event){
+  $('.sixth-slider__nav .slick-prev').click();
+});
+
+$(document).on('click', '.header-content__list.active a', function(){
+  //console.log('ff');
+  $('.header-content__list').removeClass('active');
 });
